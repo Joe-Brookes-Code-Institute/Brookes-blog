3892508d800a99e6e446a3ab29fca5ab679a964f
Joe Brookes <joe.brookes@students.codeinstitute.net>
2024-09-10 15:19:11 +0000

    commend removed

--- blog/models.py
+++ blog/models.py
@@ -15,10 +15,3 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
 
-class Comment(models.Model):
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)  # Ensure you have a Post model defined
-    text = models.TextField()
-
-
-    def __str__(self):
-        return self.text
